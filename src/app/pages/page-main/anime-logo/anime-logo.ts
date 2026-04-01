11c0535901cf869bb69db117685cad4f7258d970
Alexander <valverin95@gmail.com>
2026-04-02 01:00:26 +0300

    feat: new animations example

--- src/app/pages/page-main/anime-logo/anime-logo.ts
+++ src/app/pages/page-main/anime-logo/anime-logo.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, ElementRef, linkedSignal, signal, viewChild } from '@angular/core';
+import { animate, spring } from 'animejs';
 import { AnimeEnter, AnimeLeave } from 'ngx-animejs';
 
 @Component({
@@ -9,5 +10,34 @@ import { AnimeEnter, AnimeLeave } from 'ngx-animejs';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AnimeLogo {
-  show = signal(false);
+  readonly show = signal(true);
+  readonly el = viewChild<ElementRef<HTMLElement>>('animeEl');
+  readonly rotations = linkedSignal<boolean, number>({
+    source: this.show,
+    computation: (isShow, previous) => (!isShow ? 0 : (previous?.value ?? 0)),
+  });
+
+  readonly loopAnime = {
+    scale: [
+      { to: 1.25, ease: 'inOut(3)', duration: 200 },
+      { to: 1, ease: spring({ bounce: 0.7 }) },
+    ],
+    loop: true,
+    loopDelay: 250,
+  };
+
+  constructor() {
+    effect(() => {
+      const rotations = this.rotations();
+      const el = this.el()?.nativeElement;
+
+      if (el && rotations) {
+        animate(el, {
+          rotate: rotations * 360,
+          ease: 'out(4)',
+          duration: 1500,
+        });
+      }
+    });
+  }
 }
